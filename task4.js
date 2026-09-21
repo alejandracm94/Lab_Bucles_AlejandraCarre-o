51247eb89640518b37a7e5a147a1e4e7045ce7c2
alejandracm94 <147079835+alejandracm94@users.noreply.github.com>
2026-09-21 11:30:30 -0500

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,8 @@
+// Consulta la Tarea 4 en tus Instrucciones para completar esta tarea
+for (let i = 1; i <= 105; i++) {
+  let salida = "";
+  if (i % 3 === 0) salida += "Fizz";
+  if (i % 5 === 0) salida += "Buzz";
+  if (i % 7 === 0) salida += "Woof";
+  console.log(salida === "" ? i : salida);
+}
